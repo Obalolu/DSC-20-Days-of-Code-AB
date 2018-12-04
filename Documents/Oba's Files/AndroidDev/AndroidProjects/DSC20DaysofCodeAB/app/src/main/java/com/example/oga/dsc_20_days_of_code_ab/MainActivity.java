e15493743952040e73b68623f883fa55bfa6a36f
Busola Okeowo <busolaokemoney@gmail.com>
2018-12-04 12:48:31 +0000

    third commit

---
+++ Documents/Oba's Files/AndroidDev/AndroidProjects/DSC20DaysofCodeAB/app/src/main/java/com/example/oga/dsc_20_days_of_code_ab/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.oga.dsc_20_days_of_code_ab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
